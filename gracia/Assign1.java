f4df64326ab57df6c2857fd50f5f90bfbe8cbd75
gracia-betty-j <89856430+gracia-betty-j@users.noreply.github.com>
2021-08-31 20:08:34 +0530

    Add files via upload

---
+++ gracia/Assign1.java
@@ -0,0 +1,10 @@
+package gracia;
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ gracia/Assign2.java
@@ -0,0 +1,19 @@
+package gracia;
+
+public class Assign2 {
+
+	public static void main(String[] args) {
+			boolean a = false;
+			byte b = 10;
+			int c= 34;
+			float d = 67.2f;
+			double e = 12.3;
+			char f = 'A';
+			System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+		}
+
+	}
+
+	
+
+
